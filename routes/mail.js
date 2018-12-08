7e830c7da5ec8fb81a6811af415253082d67f0ed
unknown <dennis.alonzo.johnson@gmail.com>
2018-12-08 18:32:03 -0500

    yes

--- routes/mail.js
+++ routes/mail.js
@@ -1,16 +1,11 @@
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser');
 sendgrid = require('@sendgrid/mail');
-sendgrid.setApiKey('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');
-
-sendgrid.send({
-  to: 'dennis.alonzo.johnson@gmail.com',					//receiver's email
-  from: "dennis.alonzo.johnson@gmail.com",			//sender's email
-  subject: 'HD Track-It',				//Subject
-  text: 'Shit work yo'		//content
-}, function(err, json){
-  if (err) { return console.error(err);}
-  console.log(json);
+sendgrid.setApiKey('na');
+router.use(bodyParser.urlencoded({ extended: false}));
+router.get('/', function(req, res, next) {
+  res.send('mail stuff');
 });
 
 
@@ -18,7 +13,26 @@ router.post('/', function(req, res, next) {
   console.dir(req);
   //res.json(req.body.username);
   res.render('mail', {name: req.body.username});
+  var user=req.body.username;
+  var email=req.body.email;
+  var phone=req.body.phone;
+  var request=req.body.request;
+  var department=req.body.department;
+  var sev=req.body.sev;
+  sendgrid.send({
+    to: 'dennis.alonzo.johnson@gmail.com',					//receiver's email
+    from: 'hd@hd.com',			//sender's email
+    subject: "HD Request 1 - " + department + " " + sev, 				//Subject
+    text: 'and finally' + user + 'test',		//content
+    html: 'User: ' + user + '<br>'
+     + 'Department: ' + department + '<br>'
+     + 'Phone: ' + phone + '<br>'
+     + 'Email: ' + email + '<br>'
+     + 'Request: ' + request + '<br>',			//HTML content
+  });
 });
+
+
 
 
 
